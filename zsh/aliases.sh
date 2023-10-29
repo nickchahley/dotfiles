98b97577deba0206f76d984b272c380156bb5eb8
nickchahley <nickchahley@gmail.com>
2023-10-29 11:40:27 -0400

    clip aware of wayland/x'

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -1,127 +1,93 @@
 ### nikoli shell aliases
-alias aliases='vim ~/.config/zsh/aliases.sh'
-alias realias='source ~/.config/zsh/aliases.sh'
-alias whichdistro='lsb_release -a'
-alias pubip='curl ifconfig.me'
-alias prename='perl-rename'
-#alias thefuck='fuck'
 alias vim='nvim' # Muscle memory is still vim
-alias cvim='~/.config/nvim'
-alias swc='nvim ~/.config/sway/config'
+alias realias='source ~/.config/zsh/aliases.sh'
+alias aliases='vim ~/.config/zsh/aliases.sh && realias'
+alias ra='realias'
+alias al='aliases'
+alias whichdistro='lsb_release -a'
+alias prename='perl-rename'
+alias nrename='rename -n' # Up+home+del to actually run the rename
+#alias thefuck='fuck'
 
-# Git
-# copy the hash of the previous commit to system clipboard
-alias lastci='git rev-parse --short HEAD | xclip -selection clipboard; echo SHA of last commit copied to clipboard'
-alias editlastci='git rebase -i $(git rev-parse HEAD)'
-
-# big tmp but not tmp
-alias mgid='echo 260865097 | xclip -selection clipboard; echo McGill ID copied to clipboard'
-
-# xrandr
-alias screen_ds="xrandr --output eDP-1 --scale 0.83x0.83"
-alias screen_dpl="xrandr --output DP-1 --mode 1920x1200 --scale 2x2 --pos 0x0 --output eDP-1 --pos 3840x1200 && ~/.fehbg"
-alias screen_dpu="xrandr  --output eDP-1 --pos 0x2400 --primary --output DP-1 --mode 1920x1200 --scale 2x2 --pos 0x0 && ~/.fehbg"
-alias labup="xrandr  --output eDP-1 --pos 0x2200 --primary --output DP-1 --mode 1920x1080 --scale 2x2 --pos 0x0 && ~/.fehbg"
-alias lableft="xrandr --output DP-1 --mode 1920x1080 --scale 2x2 --pos 0x0 --output eDP-1 --pos 3840x1080 && ~/.fehbg"
-
-# Nouveau Riche
-alias scrot='scrot --select'
-alias mu='mupdf'
-alias fehf='feh --bg-fill'
-alias feht='feh --bg-tile'
+alias ...='cd ../../'
+alias ....='cd ../../../'
+alias mkdirs='mkdir -p'
+alias mine='chown -R nikoli:nikoli'
+alias has='touch'
+alias makework='chmod +x'
+alias work='chmod +x'
+alias pls='chmod +x'
+alias please='chmod -R 770'
+alias everyones='chmod -R 777'
+alias tarzip='tar -czvf'
+alias cp='cp -v'
+alias mv='mv -v'
+alias so='source'
+alias rp='realpath'
+alias rl='readlink'
+alias lsd='ls -d' # List directories only (ex. lsd L*)
+alias duh='du -h'
 alias rsync='rsync --info=progress2'
 alias rsync_changes='rsync --info=progress2 --delete -r'
+alias sudoe='sudo -E'
+alias sudoedit='sudo -E nvim'
 alias isudo='sudo -i'
-alias duh='du -h'
+
 alias myip='dig +short myip.opendns.com @resolver1.opendns.com'
 alias whatmyip='dig +short myip.opendns.com @resolver1.opendns.com'
-alias findi='find -iname' 
-alias fgre'find * -regextype egrep -regex'
-alias onlysplitlines="IFS=$'\n'"
-alias osl="IFS=$'\n'"
+alias pubip='curl ifconfig.me'
 
-# Dualshock 4
-alias ds4='ds4drv --hidraw --led 000000'
-alias ds4b='ds4drv --hidraw --led 00ffff'
-
-# :(
-alias encourage='cat "/home/nikoli/Documents/Nice Words/micheal_blough"'
 
 # TODO learn to use this crazy shit
-# for the pushd and popd commands that we shoud use but don't understand
+# for the pushd and popd commands that I shoud use but don't understand
 alias dirs='dirs -v'
 
-# set default audio dev to bluetooth headphones (airpods)
-alias pablu='paswitch blue'
-alias pablue='paswitch blue'
+# Navigation 
+alias nod='cd /home/nikoli/projects/Preprocessing/nodes/protein'
+alias pro='cd ~/projects'
+alias doc='cd ~/documents'
+alias reports='cd ~/documents'
+alias dat='cd ~/data'
+alias data='cd ~/data'
+alias db='cd ~/data/Databases'
+alias dot="cd ~/.dotfiles"
+alias con='cd ~/.config'
+alias lbin="cd ~/.local/bin"
+alias lb="cd ~/.local/bin"
+alias lshare="cd ~/.local/share"
+alias lsh="cd ~/.local/share"
+alias repos="cd ~/.local/repos"
+alias lr="cd ~/.local/repos"
 
-# Xclip: pipe output to clipboards {{{
-	#		`v` executes v cmd first and then substitutes it in place
-	#		ex:  
-	#			pwd | c
-	#			cd `v`
-	alias c='xclip'                       # X clipboard only
-	alias C='xclip -selection clipboard' # System clipboard
-	alias v='xclip -o'
-	alias V='xclip -o -selection clipboard'
-
-	# symbolic link the clipboard item
+# Terminal clipboard utilites {{{
+# `p` executes v cmd first and then substitutes it in place
+# pwd | y %% echo `p` 
+if [ $XDG_SESSION_TYPE == 'wayland' ]; then
+	alias y='wl-copy'                      
+	alias p='wl-paste'
+	alias lns='ln -s "`wl-paste`"'
+	alias lnp='linktopaste'
+	alias cdp='cd "`wl-paste`"'
+	alias mvc='mv "`wl-paste`" ./'
+	alias ep='nvim `wl-paste`'
+else
+	alias y='xclip -selection clipboard'  # System clipboard
+	alias p='xclip -o -selection clipboard'
 	alias lns='ln -s "`xclip -o`"'
-
-	# other operations involving the clipboard item
-	alias ccd='cd "`xclip -o`"'
-	alias cdc='cd "`xclip -o`"'
+	alias cdp='cd "`xclip -o`"'
 	alias mvc='mv "`xclip -o`" ./'
+	alias ep='nvim `xclip -o`'
+fi
+
 # }}}
 
-# rice {{{
-	alias pymux='pymux --truecolor'
-	alias bbre='sudo /etc/init.d/bitlbee restart'
-	alias dico='~/.local/bin/dico.sh'
-	alias fbt='feh --bg-tile'
-	alias fbf='feh --bg-fill'
-	alias tcf='vim ~/.config/terminator/config'
-	alias keycodes="xev | sed -ne '/^KeyPress/,/^$/p'"
+# Git {{{
+# copy the hash of the previous commit to system clipboard
+alias gitlastci='git rev-parse --short HEAD | xclip -selection clipboard; echo SHA of last commit copied to clipboard'
+alias giteditlastci='git rebase -i $(git rev-parse HEAD)'
+# }}}
 
-	# openbox relevant
-	alias obtheme='/home/nikoli/.local/share/themes'
-	alias ob='cd ~/.config/openbox/'
-	alias ocf='cd ~/.config/openbox/'
-	alias orc='vim ~/.config/openbox/rc.xml'
-	alias obrc='vim ~/.config/openbox/rc.xml'
-	alias xrc='vim ~/.xsessionrc'
-	alias autostart='vim ~/.config/openbox/autostart'
-	alias fehbg='feh --bg-fill'
-	alias aff='apt-file find'
-	alias afl='apt-file list'
-	alias afs='apt-file show' # alias for list
-	alias ttt='cd /home/nikoli/.config/tint2/themes'
-	alias tlpui='tlpui_run.sh'
-	alias fixesc='setxkbmap -option caps:escape'
-# }}}
-# Navigation - cd aliases {{{
-	alias dot="cd ~/.dotfiles"
-	alias lbin="cd ~/.local/bin"
-	alias lshare="cd ~/.local/share"
-	alias lsh="cd ~/.local/share"
-	alias lsrc="cd ~/.local/src"
-	alias lsr="cd ~/.local/src"
-	alias cbin="cd ~/.local/bin/cx"
-	## Programming idioms and boilerplate templates
-# }}}
 # Shell convinience aliases -- isn't that what these this whole file is? {{{
-	alias tarzip='tar -czvf'
-	alias sudoe='sudo -E'
-	alias sudoedit='sudo -E nvim'
-	alias cp='cp -v'
-	alias mv='mv -v'
-	alias gwd="echo ${PWD/#$HOME/'~'}" # pwd result w/ '~'
-	alias so='source'
-	alias rpath='realpath'
-	alias rp='realpath'
-	alias rl='readlink'
-
-	alias lsd='ls -d' # List directories only (ex. lsd L*)
 # Package Management {{{
 	# dpkg {{{
 		is_installed () {
@@ -138,18 +104,19 @@ alias pablue='paswitch blue'
 		alias doihaves='is_installed_verbose'
 	# }}}
 	# Apt {{{
-		alias addrepo='sudo add-apt-repository'
-		alias badrepo='sudo add-apt-repository -r'
+		alias addppa='sudo add-apt-repository'
+		alias badppa='sudo add-apt-repository -r'
 		alias agud='sudo apt update'
 		alias agi='sudo apt install'
 		alias agis='sudo apt install -s'
 		alias agiy='sudo apt install -y'
-		alias agug='sudo apt upgrade'
+		alias agug='sudo apt update && sudo apt upgrade'
 		alias agr='sudo apt remove'
 		alias agar='sudo apt autoremove'
 		# search for by keywoard
 		alias agcs='apt-cache search'
-		alias agfs='apt-file search'
+		alias afs='apt-file search'
+		alias searchpkg='apt-file search'
 		# purge: rm package and all config files
 		alias agrp='sudo apt --purge remove'
 		alias agpr='sudo apt --purge remove'
@@ -157,25 +124,20 @@ alias pablue='paswitch blue'
 		alias agpry='sudo apt --purge remove -y'
 	# }}}
 # }}}
-	# use for dank terminal {{{
-		alias icanhas='touch'
-		alias has='touch'
-		alias donotwant='rm'
+	# use for terminal {{{
+		alias dontwant='\rm -r'
 		alias dowant='cp -r'
-		alias workpls='chmod +x'
-		alias makework='chmod +x'
-		alias kthxbai='sudo reboot'
-		alias cya='sudo shutdown now'
-		alias nh='echo "Nice hand."'
-		alias ggwp='echo "Good game, well played"' 
+		alias cya='sudo reboot'
+		alias kthxbai='sudo shutdown now'
+		# :(
+		alias encourage='cat "/home/nikoli/Documents/Nice Words/micheal_blough"'
 	# }}}
 # }}}
 # Literal Trash Commands {{{
-	# Using rm to move files to trash is like weed. It is common and pleasuring
-	# but can be bad for you in the future. 
+	# Using rm to move files to trash is like weed. It is common and pleasing but
+	# can be bad for you in the future. 
 	# \rm : bypass alias you really need to use rm
 	alias rm='echo "This is not the command you are looking for."; false'
-	# trash from command line (vs always rm) from trash-cli
 	alias tr='trash-put'
 	alias trls='trash-list'
 	alias tr-ls='trash-list'
@@ -209,21 +171,67 @@ alias pablue='paswitch blue'
 	#alias scdla='scdl --addtofile'
 	alias ytdl='youtube-dl -x --audio-format mp3'
 	alias mus='~/Music/'
-	alias music='cd ~/Music/Library/'
-	# iPod shuffle | LIVING IN THE DATABASE DATABASE DATABASE
-	alias shuffle='python ~/Slab/Music/iPod/shuffle.py /media/nikoli/IPOD'
-	# Lists of new / soon to be new music
-	alias newmus='vim ~/Music/music.new'
-	alias getmus='vim ~/Music/music.get'
+	alias music='cd ~/music/library/'
+# }}}
+# media
+# set default audio dev to bluetooth headphones (airpods)
+alias pablu='paswitch blue'
+alias pablue='paswitch blue'
+# Dualshock 4
+alias ds4='ds4drv --hidraw --led 000000'
+alias ds4b='ds4drv --hidraw --led 00ffff'
 
-	# cmus
-# how to do currently playing?
-	alias song="cmus-remote -C 'echo {}'" # path to currently SELECTED track
-# }}}
-# Help me with syntax! {{{
-	alias pdfsyntax='cat ~/linux/PDF-Manipulation/pdftk/pdftk-examples'
-	alias id3ref='cat ~/linux/Audio/id3v2-ref'
-	alias tagref='cat ~/linux/Audio/id3v2-ref'
-	alias soxclip='cat ~/linux/Audio/SoX/make-clip-with-sox'
-# }}}
+# displays backlight, monitors
+alias whichdisplays='xrandr -q | grep " connected"'
+alias whichscreens='xrandr -q | grep " connected"'
+alias light='brightnessctl set'
+alias howbright='brightnessctl'
+alias bedcoding='brightnessctl set 1'
+alias bedtime='brightnessctl set 1'
+
+alias ctla4uniprot='P16410'
+alias ctla4string='ENSP00000497102'
+alias ctla4string11='ENSP00000303939'
+
+# https://askubuntu.com/questions/1163390/how-do-i-find-version-of-intel-graphics-card-drivers-installed
+alias videoapaters="lspci -k | grep -EA3 'VGA|3D|Display'"
+alias swaywindows='swaymsg -t get_tree | less'
+
+
+# trying some replacements for default unix tools
+# see also: br can do ls stuff
+if [[ -f "$HOME/.cargo/bin/exa" ]]; then
+	alias ls='exa'
+else
+	alias ls='ls --color=auto'
+fi
+# alias ls='exa'
+# Apply the bash default aliases for ls and grep 
+alias dir='dir --color=auto'
+alias vdir='vdir --color=auto'
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+alias bat='batcat'
+
+alias cn='~/.config/nvim/lua'
+alias cnm='nvim ~/.config/nvim/lua/modules'
+alias cs='cd ~/.config/sway'
+alias cq='cd ~/.config/qutebrowser'
+alias ecq='vim ~/.config/qutebrowser/config.py'
+alias csw='nvim ~/.config/sway'
+alias es='nvim ~/.config/sway/config'
+alias esc='vim ~/.config/sway/compatible.config'
+alias eal='nvim ~/.config/alacritty/alacritty.yml'
+alias wiki='nvim +VimwikiIndex'
+alias todo='nvim +VimwikiMakeDiaryNote'
+alias diary='nvim +VimwikiMakeDiaryNote'
+
+alias whichgtk='gsettings get org.gnome.desktop.interface gtk-theme'
+alias ggdi='gsettings get org.gnome.desktop.interface'
+alias gsdi='gsettings set org.gnome.desktop.interface'
+alias color='hyprpicker -a'
 # vim: ft=bash: ts=2: sw=2: set nowrap:
