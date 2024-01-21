c84e65c0a8f17bb001aa4fb07b3d8517d73fdf99
nickchahley <nickchahley@gmail.com>
2024-01-21 17:08:58 -0500

    refactor: consolidate nvim bootstrapping a little bit

--- setup/nvim-dap-python.sh
+++ setup/nvim-dap-python.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 # Manual setup for nvim-dap-python
 
-if false; then
-	source ./nvim.sh
-fi
-
 # might require
 sudo apt install python3-venv
 
@@ -19,7 +15,4 @@ python3 -m venv debugpy
 debugpy/bin/python -m pip install debugpy
 cd "$D"
 
-# npm is required for mason-nvim (auto lsp installation)
-
-# pytest
 pip install pytest
